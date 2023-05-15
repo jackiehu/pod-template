7e78b21f97577874861dec85119689efcc71ff33
jinhu <814030966@qq.com>
2023-05-15 10:21:40 +0800

    no message

--- setup/TemplateConfigurator.rb
+++ setup/TemplateConfigurator.rb
@@ -95,7 +95,7 @@ module Pod
     end
 # 替换文件中的变量内容
     def replace_variables_in_files
-      file_names = ['POD_LICENSE', 'POD_README.md', 'NAME.podspec', '.travis.yml', podfile_path]
+      file_names = ['POD_LICENSE', 'POD_README.md', 'NAME.podspec', 'travis.yml', podfile_path]
       file_names.each do |file_name|
         text = File.read(file_name)
         text.gsub!("${POD_NAME}", @pod_name)
@@ -126,8 +126,8 @@ module Pod
     end
 
     def rename_template_files
-      FileUtils.mv "POD_README.md", "../README.md"
-      FileUtils.mv "POD_LICENSE", "../LICENSE"
+      FileUtils.mv "POD_README.md", "README.md"
+      FileUtils.mv "POD_LICENSE", "LICENSE"
       FileUtils.mv "NAME.podspec", "#{pod_name}.podspec"
     end
 
